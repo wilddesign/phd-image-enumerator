46b0a99547d52a457d6c548c5809ee88a58c0ac7
Wojciech Nogaś <wnappdv@gmail.com>
2021-02-23 21:09:34 +0100

    Update phd-install.sh
    added git installation and cloning the repo

--- phd-install.sh
+++ phd-install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
 echo "PHD-Image generation scripts install for Linux"
+sudo apt update
+sudo apt install git
+git clone https://github.com/wilddesign/phd-image-enumerator.git
+
 sudo apt install nodejs
 sudo apt install npm
 npm install potrace
